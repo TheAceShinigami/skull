89fdbb81189720529aa30621dcf251cd58a74814
thebarbershaveshimself <ace8@me.com>
2020-02-25 19:43:25 -0800

    playing around with bulletrproofs some more

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use curve25519_dalek::{ristretto::RistrettoPoint, scalar::Scalar};
-use rand::rngs::OsRng;
-use sha2::{Digest, Sha512};
-use merlin::Transcript;
 use bulletproofs::{BulletproofGens, PedersenGens, RangeProof};
+use curve25519_dalek::{ristretto::RistrettoPoint, scalar::Scalar};
+use merlin::Transcript;
+use rand::{CryptoRng, Rng};
+use sha2::{Digest, Sha512};
 
 #[derive(Copy, Clone)]
 pub enum Card {
@@ -31,10 +31,14 @@ pub fn init_gh() -> (RistrettoPoint, RistrettoPoint) {
     )
 }
 
-pub fn schnorr_sign(h: RistrettoPoint, rs: &[Scalar]) -> SchnorrSignature {
+pub fn schnorr_sign<R: CryptoRng + Rng>(
+    r: &mut R,
+    h: RistrettoPoint,
+    rs: &[Scalar],
+) -> SchnorrSignature {
     let tail = &rs[1..];
     let key = tail.into_iter().fold(rs[0], |acc, x| acc + x);
-    let k = Scalar::random(&mut OsRng);
+    let k = Scalar::random(r);
     let hasher = Sha512::new().chain((h * k).compress().as_bytes());
     let e = Scalar::from_hash(hasher);
     SchnorrSignature {
@@ -56,19 +60,31 @@ pub fn check_schnorr_signature(
     Scalar::from_hash(hasher) == sig.e
 }
 
-
+pub fn commit_deck<R: CryptoRng + Rng>(
+    r: R,
+    pc_gens: PedersenGens,
+    deck: &[Card],
+    tscript: Transcript,
+) {
+    deck.into_iter()
+        .map(|c| match c {
+            Card::Rose => 0u64,
+            Card::Skull => 255u64,
+        });
+}
 
 pub const DEFAULT_DECK: [Card; 4] = [Card::Skull, Card::Rose, Card::Rose, Card::Rose];
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::rngs::OsRng;
 
     #[test]
     fn bulletproof() {
         let pc_gens = PedersenGens::default();
-        let bp_gens = BulletproofGens::new(8, 1);
-        let secret = 0u64;
+        let bp_gens = BulletproofGens::new(64, 1);
+        let secret = 255;
         let blinding = Scalar::random(&mut OsRng);
 
         let mut prover_transcript = Transcript::new(b"doctest example");
@@ -91,6 +107,12 @@ mod tests {
                 8,
             )
             .is_ok());
-        assert!(pc_gens.commit(Scalar::zero(), blinding).compress() == committed_value); 
+
+        assert!(
+            pc_gens
+                .commit(Scalar::from(255u64), blinding)
+                .compress()
+                == committed_value
+        );
     }
 }
